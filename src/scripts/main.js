5c9f14aaa6ccb288b5ebd7ef429c653f71ae2077
Matěj Vícha <matej.vicha.023@pslib.cz>
2025-05-12 12:17:28 +0200

    nvm uz jak to popsat

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,26 @@
-import './styles/style.css'
+const images = [
+    { src: "%BASE_URL%dragster_scooter.png", caption: "ItalJet Dragster"},
+    { src: "%BASE_URL%orange_scooter.png", caption: "Oranžový skútr"},
+    { src: "%BASE_URL%blue_scooter.jpg", caption: "Modrý skútr"},
+    { src: "%BASE_URL%purple_scooter.png", caption: "Fialový skútr"},
+    { src: "%BASE_URL%red_scooter.png", caption: "Červený skútr"},
+    { src: "%BASE_URL%yellow_scooter.png", caption: "Žlutý skútr"},
+  ];
 
+  let currentIndex = 0;
+
+  function updateGallery() {
+    document.getElementById('photo-img').src = images[currentIndex].src;
+    document.getElementById('caption').textContent = images[currentIndex].caption;
+  }   
+
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateGallery();
+  }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateGallery();
+  }
