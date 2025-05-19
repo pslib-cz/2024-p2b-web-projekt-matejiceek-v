c7e433e6fe4bb7ce917cb0a665b24e5e63fa8290
Mentosak <matej.vicha.023@pslib.cz>
2025-05-19 16:01:51 +0200

    galerie-hotovo

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,9 +1,20 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-const swiper = new Swiper(".mySwiper", {
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+let swiper = null;
+
+function toggleSwiper() {
+  const isMobile = window.matchMedia('(max-width: 1023px)').matches;
+
+  if (isMobile && !swiper) {
+    swiper = new Swiper('.mySwiper', {
+      loop: true,
+      slidesPerView: 1,
+    });
+  } else if (!isMobile && swiper) {
+    swiper.destroy(true, true);
+    swiper = null;
+  }
+}
+
+toggleSwiper();
+window.addEventListener('resize', toggleSwiper);
